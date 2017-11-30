3efefb5eb036ca00ce692784b9496dd2c7cc9dce
Ihor Harahatyi <ihor.harahatyi@atomcream.com>
2017-11-30 21:56:45 +0200

    v2 works

--- backup.py
+++ backup.py
@@ -1,30 +1,168 @@
 import docker
 import re
+import os
+import time
+from datetime import datetime
+import shutil
 
 client = docker.from_env()
+TARGET_DIR = '/Users/ogir_ok/workspace/docker-backup/backup'
+STORE_DB_BACKUPS = 3
 
-print client.containers()[0]
 
-for container in client.containers():
-    print container.get('Image'), container.get('Names')[0]
-    print container.get('Mounts')
-
-class Backup(object):
+class Backup:
     def __init__(self, container):
         self.container = container
+        self.start_date = datetime.now()
 
-    def backup(self):
+    def _env(self, name, default=None):
+        env_vars = self.container.attrs['Config']['Env']
+        for var in env_vars:
+            try:
+                var_name, val = var.split('=', 1)
+            except ValueError:
+                continue
+            if var_name == name:
+                return val
+        return default
+
+    @property
+    def _network(self):
+        return [*self.container.attrs['NetworkSettings']['Networks'].keys()][0]
+
+    @property
+    def target_dir(self):
+        return os.path.join(TARGET_DIR, self.container.name)
+
+    @property
+    def target_file(self):
+        date = self.start_date.strftime('%Y-%m-%dT%H:%M:%S')
+        file = '{date}.sql'.format(date=date)
+        return os.path.join(self.target_dir, file)
+
+    def perform_backup(self):
         raise NotImplemented
 
-class MountsBackup(self):
+    def clean_old_backups(self):
+        pass
+
     def backup(self):
-        client.containers.run()
+        start = time.time()
+        print("Back up of {} with {}".format(self.container.name, self.__class__.__name__), end='...', flush=True)
+        try:
+            self.perform_backup()
+        except Exception as e:
+            print("Failed in {0:.2f}s".format(time.time() - start))
+            print(e)
+            os.unlink(self.target_file)
+            return
+
+        print("Done in {0:.2f}s".format(time.time() - start))
+        self.clean_old_backups()
+
+
+class MountsBackup(Backup):
+    """
+    Backups all volumes of container
+    """
+
+    def perform_backup(self):
+        for mount in self.container.attrs['Mounts']:
+            if mount['Mode'] == 'rw':
+                self.backup_mount(mount['Destination'])
+
+    def backup_mount(self, folder):
+        target = os.path.join(self.target_dir, folder[1:])
+        client.containers.run('ogirok/docker-backup', 'rsync -avzP {} {}'.format(folder, target), volumes_from=self.container.name, volumes={target: {'bind': target, 'mode': 'rw'}})
+
 
 class DoNotBackup(Backup):
+    """
+    Does nothing
+    """
     def backup(self):
-        return
+        print("Skipping backup of {}".format(self.container.name))
+
+
+class DBBackup(Backup):
+    cmd = None
+
+    def backup_target(self):
+        date = datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
+        return '{date}.sql'.format(date=date)
+
+    def clean_old_backups(self):
+        for remove in sorted(os.listdir(self.target_dir))[:-STORE_DB_BACKUPS]:
+            remove_path = os.path.join(self.target_dir, remove)
+            if os.path.isdir(remove_path):
+                shutil.rmtree(remove_path)
+            else:
+                os.unlink(remove_path)
+            print('Removed outdated backup', remove)
+
+    def get_environment(self):
+        return {}
+
+    def perform_backup(self):
+
+        client.containers.run(self.container.image_name, self.cmd, links={self.container.name: self.container.name},
+                              volumes={self.target_dir: {'bind': self.target_dir, 'mode': 'rw'}},
+                              environment=self.get_environment(), network=self._network)
+
+
+class PostgresBackup(DBBackup):
+    def get_environment(self):
+        return {'PGPASSWORD': self._env('POSTGRES_PASSWORD', '')}
+
+    @property
+    def cmd(self):
+        postgres_user = self._env('POSTGRES_USER', 'postgres')
+        return '/bin/bash -c "pg_dumpall -h {host} -U {user} > {backup_file}"' \
+            .format(host=self.container.name, user=postgres_user, backup_file=self.target_file)
+
+
+class MysqlBackup(DBBackup):
+    @property
+    def cmd(self):
+        mysql_root_password = self._env('MYSQL_ROOT_PASSWORD')
+
+        return (
+            "/bin/bash -c \""
+            " echo '[mysqldump]' > /tmp/my.cnf &&"
+            " echo 'host={host}' >> /tmp/my.cnf &&"
+            " echo 'user=root' >> /tmp/my.cnf &&"
+            " echo 'password=\\\"{password}\\\"' >> /tmp/my.cnf &&"
+            " mysqldump --defaults-file=/tmp/my.cnf --all-databases > {backup_file}"
+            "\""
+        ).format(host=self.container.name, password=mysql_root_password, backup_file=self.target_file)
+
+
+class MongoBackup(DBBackup):
+    @property
+    def cmd(self):
+        return "/bin/bash -c \"mongodump -h {} --out {}\"".format(self.container.name, self.target_file)
+
+
+BACKUP_CLASSES = (
+    (r'.*postgre.*', PostgresBackup),
+    (r'.*mysql.*', MysqlBackup),
+    (r'.*mongo.*', MongoBackup),
+    (r'.*redis.*', DoNotBackup),
+    (r'.*docker-backup.*', DoNotBackup),
+    (r'.*', MountsBackup),
+)
 
 
 def backup_container(container):
-    pass
+    for pattern, backup_class in BACKUP_CLASSES:
+        for tag in container.image.tags:
+            if re.match(pattern, tag):
+                container.image_name = tag
+                backup_class(container).backup()
+                return
+
+
+if __name__ == '__main__':
+    for container in client.containers.list():
+        backup_container(container)
 
